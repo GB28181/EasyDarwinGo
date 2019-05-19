48628c89a5cca63b373a20f45e2894e70c06ae18
RainInFall <lygjcsrq@163.com>
2019-05-19 21:11:43 +0800

    [refactor]using channel instead of cond locker in player send queue

--- rtsp/player.go
+++ rtsp/player.go
@@ -1,27 +1,26 @@
 package rtsp
 
 import (
-	"sync"
 	"time"
+
+	"github.com/penggy/EasyGoLib/utils"
 )
 
 type Player struct {
 	*Session
 	Pusher *Pusher
-	cond   *sync.Cond
-	queue  []*RTPPack
+	queue  chan *RTPPack
 }
 
 func NewPlayer(session *Session, pusher *Pusher) (player *Player) {
 	player = &Player{
 		Session: session,
 		Pusher:  pusher,
-		cond:    sync.NewCond(&sync.Mutex{}),
-		queue:   make([]*RTPPack, 0),
+		queue:   make(chan *RTPPack, utils.Conf().Section("rtp").Key("send_queue_length").MustInt(128)),
 	}
 	session.StopHandles = append(session.StopHandles, func() {
 		pusher.RemovePlayer(player)
-		player.cond.Broadcast()
+		close(player.queue)
 	})
 	return
 }
@@ -32,27 +31,25 @@ func (player *Player) QueueRTP(pack *RTPPack) *Player {
 		logger.Printf("player queue enter nil pack, drop it")
 		return player
 	}
-	player.cond.L.Lock()
-	player.queue = append(player.queue, pack)
-	player.cond.Signal()
-	player.cond.L.Unlock()
+	select {
+	case player.queue <- pack:
+	default:
+		logger.Printf("player queue full, drop it")
+	}
 	return player
 }
 
 func (player *Player) Start() {
 	logger := player.logger
 	timer := time.Unix(0, 0)
+	var pack *RTPPack
+	var ok bool
 	for !player.Stoped {
-		var pack *RTPPack
-		player.cond.L.Lock()
-		if len(player.queue) == 0 {
-			player.cond.Wait()
+		pack, ok = <-player.queue
+		if !ok {
+			logger.Printf("player send queue stopped, quit send loop")
+			return
 		}
-		if len(player.queue) > 0 {
-			pack = player.queue[0]
-			player.queue = player.queue[1:]
-		}
-		player.cond.L.Unlock()
 		if pack == nil {
 			if !player.Stoped {
 				logger.Printf("player not stoped, but queue take out nil pack")
